b056e87dcb293f5b1f12a71a31dc66b985037057
Soheila(Soli) <63698187+SoheilaMolaei@users.noreply.github.com>
2022-09-26 17:49:37 +0100

    Update ModelMultDgi.py

--- ModelMultDgi.py
+++ ModelMultDgi.py
@@ -6,7 +6,7 @@ import os
 import numpy as np
 
 class GCNModelVAE(nn.Module):
-    def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout,n_nodes):
+    def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout,K):
         super(GCNModelVAE, self).__init__()
         self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
         self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
@@ -14,25 +14,25 @@ class GCNModelVAE(nn.Module):
 
         self.gc3 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
         
-        self.h2 = GraphConvolution(hidden_dim2*2, hidden_dim1, dropout, act=lambda x: x)
+        self.h2 = GraphConvolution(hidden_dim1, hidden_dim1, dropout, act=lambda x: x)
         
-        self.gc4 = GraphConvolution(hidden_dim1, hidden_dim2*2, dropout, act=lambda x: x)
-        self.gc5 = GraphConvolution(hidden_dim1, hidden_dim2*2, dropout, act=lambda x: x)
+        self.gc4 = GraphConvolution(hidden_dim1, hidden_dim1, dropout, act=lambda x: x)
+        self.gc5 = GraphConvolution(hidden_dim1, hidden_dim1, dropout, act=lambda x: x)
         
         self.dc = InnerProductDecoder(dropout, act=lambda x: x)
-        self.mu_px = nn.ModuleList([GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)  for i in range(n_nodes)])
-        self.logvar_px = nn.ModuleList([GraphConvolution(hidden_dim1, hidden_dim2,dropout, act=lambda x:x)  for i in range(n_nodes)])
-        self.qz = GraphConvolution(hidden_dim1, hidden_dim2,dropout, act=lambda x:x)
+        self.mu_px = nn.ModuleList([GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)  for i in range(K)])
+        self.logvar_px = nn.ModuleList([GraphConvolution(hidden_dim1, hidden_dim2,dropout, act=lambda x:x)  for i in range(K)])
+        self.qz = GraphConvolution(hidden_dim1, K,dropout, act=lambda x:x)
         self.hidden_dim2=hidden_dim2
-        self.n_nodes=n_nodes
+        self.K=K
     def priorGenerator(self, z_w,adj,batchSize):
       # batchSize = z_w.size(0)
 
       h = torch.tanh(self.h2(z_w,adj))
       device= torch.device("cuda" if torch.cuda.is_available() else "cpu")
-      mu_px = torch.empty(batchSize , self.hidden_dim2,self.n_nodes, device=device, requires_grad=False)
-      logvar_px = torch.empty(batchSize, self.hidden_dim2,self.n_nodes, device=device, requires_grad=False)
-      for i in range(self.n_nodes):
+      mu_px = torch.empty(batchSize , self.hidden_dim2,self.K, device=device, requires_grad=False)
+      logvar_px = torch.empty(batchSize, self.hidden_dim2,self.K, device=device, requires_grad=False)
+      for i in range(self.K):
         mu_px[:,:,i] = self.mu_px[i](h,adj)
         logvar_px[:, :,i] = self.logvar_px[i](h,adj)
 
@@ -59,10 +59,13 @@ class GCNModelVAE(nn.Module):
     def forward(self, x, adj):
       batchSize = x.size(0)
       mu_x, logvar_x,mu_w, logvar_w,hidden1 = self.encode(x, adj)
-      z_x = self.reparameterize(mu_x, logvar_x)
+
+      z_x = self.reparameterize(mu_x, logvar_x)#X maqaleh
       z_w = self.reparameterize(mu_w, logvar_w)
       mu_px, logvar_px = self.priorGenerator(z_w,adj,batchSize)
-      qz = F.softmax(self.qz(hidden1,adj), dim=1)
+
+      qz = F.softmax(self.qz(hidden1,adj), dim=1)#Z maqaleh
+
       return z_x, mu_x, logvar_x,z_w, mu_w, logvar_w,mu_px, logvar_px,qz
 
 
@@ -79,28 +82,3 @@ class InnerProductDecoder(nn.Module):
         adj = self.act(torch.mm(z, z.t()))
         return adj
 
-
-class GCNModelAE(nn.Module):
-    def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
-        super(GCNModelAE, self).__init__()
-        self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
-        self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
-
-        self.h2 = GraphConvolution(hidden_dim2*2, hidden_dim1, dropout, act=lambda x: x)
-        
-        self.gc4 = GraphConvolution(hidden_dim1, hidden_dim2*2, dropout, act=lambda x: x)
-        self.gc5 = GraphConvolution(hidden_dim1, hidden_dim2*2, dropout, act=lambda x: x)
-        
-        self.dc = InnerProductDecoder(dropout, act=lambda x: x)
-        self.mu_px = nn.ModuleList([GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)  for i in range(n_nodes)])
-        self.logvar_px = nn.ModuleList([GraphConvolution(hidden_dim1, hidden_dim2,dropout, act=lambda x:x)  for i in range(n_nodes)])
-        self.qz = GraphConvolution(hidden_dim1, hidden_dim2,dropout, act=lambda x:x)
-        self.hidden_dim2=hidden_dim2
-        self.n_nodes=n_nodes
-    def encode(self, x, adj):
-        hidden1 = self.gc1(x, adj)
-        return self.gc2(hidden1, adj)
-
-    def forward(self, x, adj, encode=False):
-        z = self.encode(x, adj)
-        return z, z, None
